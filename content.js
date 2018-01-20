c0ed8e84e43e1b6baa7e4c8ae49a7d3fe27e326f
Håvard Anda Estensen <haavard.ae@gmail.com>
2018-01-20 15:31:55 -0800

    Replace bird imgs with local stored img

--- content.js
+++ content.js
@@ -1,16 +1,15 @@
 var images = this.document.getElementsByTagName('img');
 const birdFeatures=["beak", "finch", "wing", "bird", "lark", "seabird","waterbird", "water bird"];
 const url = 'https://vision.googleapis.com/v1/images:annotate?key=AIzaSyAGP_QBmE-Brt-uItNnFj2T_9QRvDRKU40';
- for (var x=0; x<images.length;x++) {
+for (var x=0; x<images.length;x++) {
+  currentImage=images[x];
 
-   currentImage=images[x];
-
- if (currentImage.src.indexOf("bird")>-1)
-currentImage.src="http://i0.kym-cdn.com/entries/icons/mobile/000/013/564/doge.jpg"
-
- else
-   processImage(currentImage);
- }
+  if (currentImage.src.indexOf("bird")>-1) {
+    currentImage.src = chrome.extension.getURL('filtered.jpg');
+  } else {
+    processImage(currentImage);
+  }
+}
 
 function processImage(img) {
 
