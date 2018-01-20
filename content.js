eecf26a45f5c9f07063faac5878e7107b28f68ae
Agatha Turyahikayo <agathaturyahikayo14@gmail.com>
2018-01-20 09:40:08 -0800

    fixed some variable names

--- content.js
+++ content.js
@@ -1,8 +1,8 @@
 var images = document.getElementsByTagName('img');
-for (var x = 0, l = images.length; i < l; i++) {
+for (var x = 0, x<images.length; x++) {
   currentImage=images[x]
   if (isBird(currentImage))
-    images[x].src = 'some wholesome image url' + images[x].width + '/' + images[x.height];
+    images[x].src = 'some wholesome image url' + images[x].width + '/' + images[x].height;
 }
 
 
